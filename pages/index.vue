dae61a0593e2a3dc20c0f5e2a1cdf1553e940fa6
nyk <0xnykcd@googlemail.com>
2022-03-23 20:26:42 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <body>
+        <div class="app container">
+            <Playground />
+        </div>
+    </body>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+body {
+    background: black;
+    height: 100%;
+    font-family: 'Libre Franklin', sans-serif;
+}
+
+  h1,
+  h2,
+  h3 {
+    font-family: 'Inter', sans-serif;
+
+  }
+.app {
+    max-width: 100%;
+    overflow: hidden;
+}
+</style>
