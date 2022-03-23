a889ec42aba0154eae30b9abc6c02e68d01d118f
nyk <0xnykcd@googlemail.com>
2022-03-23 20:37:19 +0100

    generate

---
+++ dist/_nuxt/static/1648064206/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648064206",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dd0e0a98"}}}}(null,"\u002F"));
